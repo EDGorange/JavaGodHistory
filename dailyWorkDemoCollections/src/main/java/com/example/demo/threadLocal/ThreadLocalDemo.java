3b0825893ca3b732bcde2f884a26f2bc373ba86b
EDGorange <1049621484@qq.com>
2023-07-06 11:28:15 +0800

    new article changed

--- dailyWorkDemoCollections/src/main/java/com/example/demo/threadLocal/ThreadLocalDemo.java
+++ dailyWorkDemoCollections/src/main/java/com/example/demo/threadLocal/ThreadLocalDemo.java
@@ -1,9 +1,8 @@
 package com.example.demo.threadLocal;
 
-import java.util.concurrent.Callable;
+import org.springframework.util.StringUtils;
+
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @description: br
@@ -24,6 +23,7 @@ public class ThreadLocalDemo {
         public void saleHouse() {
             this.threadLocal.set(this.threadLocal.get() + 1);
         }
+
     }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -45,7 +45,8 @@ public class ThreadLocalDemo {
             }, "t" + i).start();
         }*/
 
-        MyThreadLocal.set("3", Thread.currentThread().getName());
+        /*MyThreadLocal.set("3", Thread.currentThread().getName());
+        System.out.println(MyThreadLocal.get("3"));
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 3; i++) {
 
@@ -61,6 +62,69 @@ public class ThreadLocalDemo {
         }
 
         executorService.shutdown();
+        Thread.sleep(10000);
+        new Thread(() -> {
+            System.out.println("----" + MyThreadLocal.get("3"));
+        }).start();
+        Thread thread = new Thread("myThread11") {
+            @Override
+            public void run() {
+                System.out.println(MyThreadLocal.get("3"));
+            }
+        };
+        thread.start();
+        System.out.println("main"+  MyThreadLocal.get("3"));
+
+        System.out.println("--------------------------");
+        System.out.println("123" + null);
+        String a = null;
+        String b = "789A";
+        System.out.println("456" + Strings.nullToEmpty(b));
+        System.out.println("-===================================");
+        System.out.println(Strings.isNullOrEmpty(""));
+        System.out.println(Strings.isNullOrEmpty(null));
+        System.out.println(Strings.isNullOrEmpty(" "));
+        System.out.println("==================================================");
+        System.out.println(StringUtils.endsWithIgnoreCase(b, "A"));*/
+        String asid = "7000708663FRANCHISEE_MAINT";
+//只要字符串
+        String str = asid.replaceAll("[0-9]","");
+        System.out.println("只要字符串： " + str);
+//只要数字
+        String num = asid.replaceAll("[a-zA-Z_]","");
+        System.out.println("只要数字： " + num);
+        System.out.println("%%%%%%%%%%%%%%");
+        System.out.println(asid);
+        //System.out.println(Arrays.asList(asid.split(num)));
+        System.out.println("*********************************");
+        System.out.println(asid);
+        System.out.println(asid.split(num).length);
+        for (int i = 0; i < asid.split("700").length; i++) {
+            System.out.println(asid.split("700")[i]);
+        }
+        System.out.println(asid.split(num)[1]);
+        System.out.println(asid.split(num)[0]);
+
+
+        String test = "123ags";
+        for (String s : test.split("123")) {
+            System.out.println(s);
+        }
+        System.out.println("^^^^^^^^^^^^^^^^^^^");
+        String test2 = "898LC NEW";
+        String orderNo = test2.replaceAll("[a-zA-Z_]", "");
+        String[] split = test2.split(StringUtils.trimAllWhitespace(orderNo));
+        System.out.println(orderNo);
+        System.out.println(test2);
+        System.out.println(split[0]);
+        System.out.println(split[1]);
+
+        String test3 = "898";
+        String orderNo3 = test3.replaceAll("[a-zA-Z_]", "");
+        String[] split3 = test3.split(StringUtils.trimAllWhitespace(orderNo3));
+        System.out.println(split3.length);
+        System.out.println(split3[0]);
+        System.out.println(split3[1]);
 
     }
 
